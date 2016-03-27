b29736a80ffa35fe02e79eac56bec68cb7ec627a
CodexWorld <nityamaity87@gmail.com>
2016-03-27 19:03:15 +0530

    Create getAddress.php

---
+++ getAddress.php
@@ -0,0 +1,34 @@
+<?php
+/**
+* Author: CodexWorld
+* Author URI: http://www.codexworld.com
+* Function Name: getAddress()
+* $address => Full address.
+* Return =>  Address of the given Latitude and longitude.
+**/
+function getAddress($latitude,$longitude){
+    if(!empty($latitude) && !empty($longitude)){
+        //Send request and receive json data by address
+        $geocodeFromLatLong = file_get_contents('http://maps.googleapis.com/maps/api/geocode/json?latlng='.trim($latitude).','.trim($longitude).'&sensor=false'); 
+        $output = json_decode($geocodeFromLatLong);
+        $status = $output->status;
+        //Get address from json data
+        $address = ($status=="OK")?$output->results[1]->formatted_address:'';
+        //Return address of the given latitude and longitude
+        if(!empty($address)){
+            return $address;
+        }else{
+            return false;
+        }
+    }else{
+        return false;   
+    }
+}
+
+/**
+ * Use getAddress() function like the following.
+ */
+$latitude = '38.8976805';
+$longitude = '-77.0387185';
+$address = getAddress($latitude,$longitude);
+$address = $address?$address:'Not found';
